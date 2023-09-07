81e2228376173330d3875819c8e8319505934d01
Mac Sweeny <macsweenydev@gmail.com>
2023-09-07 22:14:24 +1000

    feat: task template

--- apps/dashboard/src/app/dashboard/layout.tsx
+++ apps/dashboard/src/app/dashboard/layout.tsx
@@ -1,10 +1,15 @@
+import React, { Suspense } from "react";
 import { redirect } from "next/navigation";
 
 import { auth } from "@task-tornado/auth";
 
 import { ThemeProvider } from "~/components/theme-provider";
+import { ModeToggle } from "~/components/ui/theme-toggle";
 import { Toaster } from "~/components/ui/toaster";
-import DashboardLayout from "./_components/dashboard-layout";
+import { MainNav } from "./_components/main-nav";
+import { Search } from "./_components/search";
+import { TeamSwitcher } from "./_components/team-switcher";
+import { UserNav } from "./_components/user-nav";
 
 export default async function Page(props: { children: React.ReactNode }) {
   const session = await auth();
@@ -13,10 +18,19 @@ export default async function Page(props: { children: React.ReactNode }) {
   }
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <DashboardLayout>
-        <main>{props.children}</main>
-        <Toaster />
-      </DashboardLayout>
+      <div className="flex h-16 items-center border-b bg-background px-4">
+        <Suspense>
+          <TeamSwitcher />
+        </Suspense>
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <Search />
+          <ModeToggle />
+          <UserNav />
+        </div>
+      </div>
+      <main>{props.children}</main>
+      <Toaster />
     </ThemeProvider>
   );
 }
--- apps/dashboard/src/app/dashboard/page.tsx
+++ apps/dashboard/src/app/dashboard/page.tsx
@@ -1,7 +1,54 @@
-export default function Page() {
+import { promises as fs } from "fs";
+import path from "path";
+import type { Metadata } from "next";
+import Image from "next/image";
+import { z } from "zod";
+
+import { auth } from "@task-tornado/auth";
+
+import { taskSchema } from "./_components/_schema";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { UserNav } from "./_components/user-nav";
+
+export const metadata: Metadata = {
+  title: "Tasks",
+  description: "A task and issue tracker build using Tanstack Table.",
+};
+
+// Simulate a database read for tasks.
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "/src/app/dashboard/_components/_tasks.json"),
+  );
+
+  // const tasks = JSON.parse(data.toString());
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
+}
+
+export default async function TaskPage() {
+  const session = await auth();
+  const tasks = await getTasks();
+
   return (
-    <div>
-      <div>test</div>
-    </div>
+    <>
+      <div className=" h-full flex-1 flex-col space-y-8 p-8 ">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">
+              Welcome back {""}
+              {session?.user?.name?.split(" ")[0]}!
+            </h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
   );
 }
