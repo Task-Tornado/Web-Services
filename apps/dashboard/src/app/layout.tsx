4097dfe3052864d98af31d2d7b82138e884b9edc
Mac Sweeny <macsweenydev@gmail.com>
2023-09-07 11:18:10 +1000

    feat: session middleware

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -5,9 +5,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { headers } from "next/headers";
 
-import { ThemeProvider } from "~/components/theme-provider";
-import { Toaster } from "~/components/ui/toaster";
-import DashboardLayout from "./_components/dashboard-layout";
 import { TRPCReactProvider } from "./providers";
 
 const fontSans = Inter({
@@ -31,12 +28,7 @@ export default function Layout(props: { children: React.ReactNode }) {
     <html lang="en">
       <body className={["font-sans", fontSans.variable].join(" ")}>
         <TRPCReactProvider headers={headers()}>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <DashboardLayout>
-              <main>{props.children}</main>
-              <Toaster />
-            </DashboardLayout>
-          </ThemeProvider>
+          <main>{props.children}</main>
         </TRPCReactProvider>
       </body>
     </html>
