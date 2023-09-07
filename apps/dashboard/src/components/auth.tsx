361eae4efa36607867b1f6ca4841ac67b26c8759
Mac Sweeny <macsweenydev@gmail.com>
2023-09-07 14:19:15 +1000

    feat: google auth button

--- apps/dashboard/src/components/auth.tsx
+++ apps/dashboard/src/components/auth.tsx
@@ -1,7 +1,11 @@
 import type { ComponentProps } from "react";
+import Image from "next/image";
 
 import { CSRF_experimental } from "@task-tornado/auth";
 import type { OAuthProviders } from "@task-tornado/auth";
+
+import Google from "~/assets/icons/google.svg";
+import { Button } from "./ui/button";
 
 export function SignIn({
   provider,
@@ -19,6 +23,22 @@ export function SignOut(props: ComponentProps<"button">) {
   return (
     <form action="/api/auth/signout" method="post">
       <button {...props} />
+      <CSRF_experimental />
+    </form>
+  );
+}
+export function GoogleAuth() {
+  return (
+    <form action="/api/auth/signin/google" method="POST">
+      <Button className="flex w-full gap-x-2 bg-neutral-950 hover:bg-neutral-800">
+        <Image
+          className="h-5 w-auto"
+          src={Google as string}
+          loading="lazy"
+          alt="google logo"
+        />
+        Continue with Google
+      </Button>
       <CSRF_experimental />
     </form>
   );
